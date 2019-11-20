816fab1dde60ca3b025f2082f18fb9a2693977c0
ElDuddz <elduddz@outlook.com>
2019-11-20 14:44:02 +0000

    try different options to get it working

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,12 +9,10 @@ data "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_app_service_plan" "sp" {
-  name                = "${var.webservice_name}-plan"
+  name                = "${var.webservice_name}-service-plan"
   location            = "${data.azurerm_resource_group.rg.location}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  kind                = "Linux"
-  reserved            = true
-
+  
   sku {
     tier = "Standard"
     size = "S1"
