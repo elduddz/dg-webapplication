bcfe4695e3e90f1b1450bc402828a8198c8590f5
ElDuddz <elduddz@outlook.com>
2019-12-12 22:57:16 +0000

    fixing syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,22 +8,17 @@ data "azurerm_resource_group" "rg" {
   name = "dependencygraph-rg"
 }
 
-resource "azurerm_app_service_plan" "sp" {
-  name                = "${var.webservice_name}-service-plan"
-  location            = "${data.azurerm_resource_group.rg.location}"
-  resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  
-  sku {
-    tier = "Standard"
-    size = "B1"
-  }
+data "azurerm_app_service_plan" "sp" {
+  name                = "asp-elduddz"
+  resource_group_name = "UKW-elduddz"
+
 }
 
 resource "azurerm_app_service" "appvc" {
-  name                = "${var.webservice_name}"
-  location            = "${data.azurerm_resource_group.rg.location}"
-  resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.sp.id}"
+  name                = var.webservice_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = data.azurerm_app_service_plan.sp.id
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -31,14 +26,14 @@ resource "azurerm_app_service" "appvc" {
   }
 
   app_settings = {
-    "dependencygraphurl" = "${var.dependency_graph_url}"
-    "code" = "${var.dependency_graph_code}"
+    "dependencygraphurl" = var.dependency_graph_url
+    "code"               = var.dependency_graph_code
   }
 }
 
 resource "azurerm_application_insights" "appInsight" {
   name                = "${var.webservice_name}-appInsight"
-  resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  location            = "${data.azurerm_resource_group.rg.location}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   application_type    = "Other"
 }
